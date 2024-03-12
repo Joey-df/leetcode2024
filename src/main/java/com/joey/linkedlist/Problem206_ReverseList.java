27867abe3e8180def8a03272850f869c232a0c39
liupei <liupei@yidian-inc.com>
2024-03-12 15:10:42 +0800

    Initial commit

---
+++ src/main/java/com/joey/linkedlist/Problem206_ReverseList.java
@@ -0,0 +1,13 @@
+package com.joey.linkedlist;
+
+/**
+ * @author pei.liu
+ */
+
+//206. 反转链表
+public class Problem206_ReverseList {
+
+    public static ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
---
+++ src/main/java/com/joey/linkedlist/Problem21_MergeTwoLists.java
@@ -0,0 +1,14 @@
+package com.joey.linkedlist;
+
+/**
+ * @author pei.liu
+ */
+
+//21. 合并两个有序链表
+public class Problem21_MergeTwoLists {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+
+}
