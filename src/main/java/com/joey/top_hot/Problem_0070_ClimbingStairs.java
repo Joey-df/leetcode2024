d174db8c71f1f903a756da21ea401681a348884c
liupei <461711602@qq.com>
2024-05-13 21:39:22 +0800

    mod

--- src/main/java/com/joey/top_hot/Problem_0070_ClimbingStairs.java
+++ src/main/java/com/joey/top_hot/Problem_0070_ClimbingStairs.java
@@ -1,5 +1,8 @@
 package com.joey.top_hot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 70. 爬楼梯
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
@@ -34,5 +37,22 @@ public class Problem_0070_ClimbingStairs {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+    //总共n步要走，还有rest步要走
+    //沿途的路径存在path里
+    //ans收集答案
+    public static void f(int n, int rest, List<String> path, List<List<String>> ans) {
+        if (rest < 0) return;
+        if (rest == 0) {
+            ans.add(new ArrayList<>(path));
+        } else { //rest>=1
+            path.add("1阶");
+            f(n, rest - 1, path, ans);
+            path.remove(path.size() - 1);
+            path.add("2阶");
+            f(n, rest - 2, path, ans);
+            path.remove(path.size() - 1);
+        }
     }
 }
