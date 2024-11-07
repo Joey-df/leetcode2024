ef8bcbc0eeb871553a214ec84149606ba2544005
liupei <461711602@qq.com>
2024-11-07 23:34:30 +0800

    mod

--- src/main/java/com/joey/leetcode/Problem_2563_CountTheNumberOfFairPairs.java
+++ src/main/java/com/joey/leetcode/Problem_2563_CountTheNumberOfFairPairs.java
@@ -1,7 +1,6 @@
 package com.joey.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /**
@@ -26,16 +25,127 @@ import java.util.TreeMap;
 //nums.length == n
 //-10^9 <= nums[i] <= 10^9
 //-10^9 <= lower <= upper <= 10^9
+//二分查找
 public class Problem_2563_CountTheNumberOfFairPairs {
 
-    public long countFairPairs(int[] nums, int lower, int upper) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        //TODO
-        //lower <= nums[i] + nums[j] <= upper
-        //lower-nums[i] <= nums[j] <= upper-nums[i]
-        //到每个位置，求小于当前数的key有几个，大于当前key的数有几个？
-        return 0;
+    //lower <= nums[i] + nums[j] <= upper
+    //即任意两个不同位置之和位于[low,up]范围上，所以和顺序无关，可以排序
+    //设nums[i] + nums[j] = sum
+    //转化为 count(sum <= upper) - count(sum <= lower-1)
+    public static long countFairPairs(int[] nums, int lower, int upper) {
+        return fun(nums, upper) - fun(nums, lower - 1);
+    }
+
+    //给定数组arr，求累加和<=target的数对有几个
+    public static long fun(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int l = 0;
+        int r = n - 1;
+        long ans = 0;
+        while (l < r) {
+            int sum = arr[l] + arr[r];
+            if (sum <= target) {
+                ans += r - l; //以i做第一个数的数对个数
+                l++; //看下一个位置
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+
+    //TODO 复习
+    //使用排序+treeMap 实现 <=某个数最右的位置，计算区间问题
+    public static long countFairPairs2(int[] nums, int lower, int upper) {
+        long ans = 0;
+        //<当前数，当前数最后出现的位置>
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        treeMap.put(Integer.MIN_VALUE, -1);//兜底用的
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) { //对每一个位置i求一个答案
+            int x = nums[i]; //固定一个数，求另一个出现的范围
+            int minAdd = lower - x;
+            int maxAdd = upper - x;
+
+            //小于等于 minAdd-1 这个key最后出现的位置（即<=某个数最右的位置）
+            int left = treeMap.floorEntry(minAdd - 1).getValue();
+            //小于等于 maxAdd 这个key最后出现的位置（即<=某个数最右的位置）
+            int right = treeMap.floorEntry(maxAdd).getValue();
+            ans += right - left;
+
+            treeMap.put(x, i);
+        }
+
+        return ans;
+    }
+
+    //lower <= nums[i] + nums[j] <= upper
+    //任意两个i,j只要满足以上条件就可以，因为结果是统计数量，任意一个位置和哪些位置结合能产生答案是确定的，并不会因为排序产生变化，so，和顺序无关，
+    //lower - nums[j] <= nums[i] <= upper - nums[j]
+    //即到任意一个j位置，求[0,j-1]范围上哪些数落在[ lower - nums[j], upper - nums[j] ]范围上
+    //即以nums[j]作为第二个数姿态的情况下，在此之前有多少个数满足条件
+    //即在[0,j-1]上找 >=lower - nums[j]最左的位置
+    //在[0,j-1]上找 <=upper - nums[j]最右的位置
+    public static long countFairPairs3(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            int a = lower - nums[i];
+            int b = upper - nums[i];
+            int l = findLeft(nums, 0, i - 1, a);
+            int r = findRight(nums, 0, i - 1, b);
+            System.out.printf("l=%d, r=%d\n", l, r);
+            ans += (l == -1 || r == -1) ? 0 : r - l + 1;
+        }
+        return ans;
+    }
+
+    public static int findLeft(int[] arr, int l, int r, int target) {
+        int ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] >= target) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans; //-1表示没找到
+    }
+
+    public static int findRight(int[] arr, int l, int r, int target) {
+        int ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] <= target) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans; //-1表示没找到
+    }
+
+    public static long countFairPairs4(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            int a = lower - nums[i];
+            int b = upper - nums[i];
+            int l = findRight(nums, 0, i - 1, a - 1);
+            int r = findRight(nums, 0, i - 1, b);
+            //System.out.printf("l=%d, r=%d\n", l, r);
+            ans += r - l; // <=up - <=low-1 =>[low,up]
+        }
+        return ans;
     }
 
 }
--- src/main/java/com/joey/leetcode/Problem_2824_CountPairsWhoseSumIsLessThanTarget.java
+++ src/main/java/com/joey/leetcode/Problem_2824_CountPairsWhoseSumIsLessThanTarget.java
@@ -1,6 +1,8 @@
 package com.joey.leetcode;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author pei.liu
@@ -18,9 +20,37 @@ import java.util.List;
 //注意 (0, 3) 不计入答案因为 nums[0] + nums[3] 不是严格小于 target
 public class Problem_2824_CountPairsWhoseSumIsLessThanTarget {
 
-    public int countPairs(List<Integer> nums, int target) {
-        //TODO
-        return 0;
+    //求满足0 <= i < j < n 且 nums[i] + nums[j] < target 的下标对 (i, j) 的数目。
+    //转化为：在任意一个位置i求前面多少个位置的数？满足 nums[j] < target - nums[i]，求数对数量所以和位置无关，可以排序
+    //转化为：二分查找 小于m最右的位置，假设该位置为p，则位置i的答案为：p+1，即[0，p]这些位置和i位置结合都是满足条件的
+    public static int countPairs(List<Integer> nums, int target) {
+        int[] arr = nums.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(arr);
+        // -7 -6 -2 -1 2 3 5
+        int n = arr.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int num = target - arr[i];
+            //在[0,i-1]范围上找 < num最右的位置
+            int p = findRight(arr, 0, i - 1, num);
+            ans += p+1;
+        }
+        return ans;
     }
+
+    public static int findRight(int[] arr, int l, int r, int target) {
+        int ans = -1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] < target) { //求小于这里就是arr[m] < target，如果求<=左右的位置就改为 arr[m] <= target
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+
 
 }
